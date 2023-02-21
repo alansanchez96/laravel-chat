ed64f40b644a8002f10ec2b2d098fe250907bbec
Alan Sanchez <alaansannchezz@gmail.com>
2023-02-21 16:48:26 -0300

    Se finalizo entorno de configuracion de la app

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -30,3 +30,16 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
 //     enabledTransports: ['ws', 'wss'],
 // });
+
+import Echo from "laravel-echo"
+
+window.Pusher = require('pusher-js');
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    wsHost: window.location.hostname,
+    wsPort: 6001,
+    forceTLS: false,
+    disableStats: true,
+});
